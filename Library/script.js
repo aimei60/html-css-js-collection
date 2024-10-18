8ab413a0674df0f4f89709826c70abfafc599cf0
aimei60 <104590763+aimei60@users.noreply.github.com>
2024-10-18 15:02:07 +0100

    Designed my library and added my example book cards

---
+++ Library/script.js
@@ -0,0 +1,27 @@
+const myLibrary = [{
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 320,
+    read: true,
+    id: 1,
+}, {
+    title: "Wuthering Heights",
+    author: "Emily Bronte",
+    pages: 401,
+    read: false,
+    id: 2,
+}];
+
+function Book(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+
+
+
+
+
